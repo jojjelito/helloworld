a1cc77f8b3591539843b1230519fac053fdbd00e
test <test@burk.(none)>
2013-07-12 22:45:09 +0200

    m

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 def hello(request):
 	p=conlog(who='someone',when=timezone.now())
 	p.save()
-	html="<html><body><p>Hello World!</p>"
+	html="<html><title>test django project</title>q<body><p>Hello World!</p>"
 	for entry in conlog.objects.all():
 		html=html+'<p>'+entry.who+' connected at ' + str(entry.when)+'</p>'
 	html=html+"</body></html>"		
