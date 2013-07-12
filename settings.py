15991c51aec69cd1386a42dcaa85b290c2e81e6c
test <test@burk.(none)>
2013-07-12 04:34:52 +0200

    m

--- settings.py
+++ settings.py
@@ -8,18 +8,16 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dbfile',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+        'NAME': 'packaginator',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'packaginator',
+        'PASSWORD': 'awesome_password',
+        'HOST': '',
+        'PORT': '',
+    },
 }
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
