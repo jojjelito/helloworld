6ad4933dd749233c9acb7efb4c5305cbc9b5a753
Johan Vallander <johan.vallander@gmail.com>
2013-07-12 03:36:40 +0200

    added missed comma

--- settings.py
+++ settings.py
@@ -123,8 +123,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'gunicorn'
-    'helloworld'
+    'gunicorn',
+    'helloworld',
 )
 
 # A sample logging configuration. The only tangible logging
